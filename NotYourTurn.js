894a3b72afa0992ff31aba2266c1c2743e4c0cfa
CDeenen <cdeenen@outlook.com>
2020-09-13 22:31:03 +0200

    v1.0.0

---
+++ NotYourTurn.js
@@ -0,0 +1,365 @@
+/* 
+ * NotYourTurn
+ * Author: Cris#6864
+ */
+
+new Date();
+let timer = 0; 
+let role;
+let blockSett;
+let duplicateCheck = false;
+let oldPositionX;
+let oldPositionY;
+
+function checkCombat(){
+    if (game.combat) 
+        return game.combat.data.active;
+    else return false; 
+}
+
+function whisperGM(message){
+    for (let i=0; i<game.data.users.length; i++){
+        if (game.data.users[i].role > 2) 
+            ChatMessage.create({
+                content: message,
+                whisper: [game.data.users[i]._id]
+        });                                                                                      
+    }
+}
+
+Hooks.on('ready', ()=>{
+    role =  game.user.data.role;
+    if (role == 1) blockSett = game.settings.get("NotYourTurn","BlockPlayer");
+    else if (role == 2) blockSett = game.settings.get("NotYourTurn","BlockTrusted");
+    else if (role == 3) blockSett = game.settings.get("NotYourTurn","BlockAssistant");
+    else if (role == 4) blockSett = game.settings.get("NotYourTurn","BlockGM");
+    timer = Date.now();
+
+    game.socket.on(`module.NotYourTurn`, (payload) =>{
+        //check if this user is the target, else return
+        if (game.userId != payload.receiver) return;
+
+        //check if the correct message has been received
+        if (payload.msgType == "requestMovement"){
+        
+            //get the name of the requesting user, and his/her token data
+            let user = game.users.get(payload.sender).data.name;
+            let token;
+            for (let i=0; i<canvas.tokens.children[0].children.length; i++)
+                if (canvas.tokens.children[0].children[i].data._id == payload.tokenId) token = canvas.tokens.children[0].children[i];
+            
+            //build dialog
+            let applyChanges = 0;
+            let buttons = {
+            //Accept button, accepts the request
+                Accept: {
+                    label: `Accept`,
+                    callback: () => applyChanges = 0
+                }
+            }
+            //Decline button, declines the request
+            buttons.Decline = {
+                label: `Decline`,
+                callback: () => applyChanges = 2
+            }
+            
+            let d = new Dialog({
+                title: `Movement request`,
+                content: `Player '` + user + `' has requested to move '` + token.data.name,
+                buttons,
+                default: "Decline",
+                close: html => {
+                    //If 'Accept' is pressed, do nothing (accepts the movement)
+                    let ret;
+                    if (applyChanges == 0){
+                        timer = Date.now();
+                        ret = true;
+                    }
+                    //If 'Decline' is pressed, move token back to old location
+                    else if (applyChanges == 2) { //Reject
+                        token.shiftPosition((payload.oldX - token.data.x)/canvas.dimensions.size,(payload.oldY - token.data.y)/canvas.dimensions.size,true);
+                        timer = Date.now();
+                        ret = false;
+                    }
+                    let payload2 = {
+                        "msgType": "requestMovement_GMack",
+                        "sender": game.userId, 
+                        "receiver": payload.sender, 
+                        "tokenId": token.data._id,
+                        "oldX": payload.oldX,
+                        "oldY": payload.oldY,
+                        ret
+                    };
+                    game.socket.emit(`module.NotYourTurn`, payload2);
+                }
+            });
+            d.render(true);
+        }
+        else if (payload.msgType == "requestMovement_GMack"){
+            let token;
+                for (let i=0; i<canvas.tokens.children[0].children.length; i++)
+                    if (canvas.tokens.children[0].children[i].data._id == payload.tokenId) token = canvas.tokens.children[0].children[i];
+            if (payload.ret == true) {
+                ui.notifications.info("The GM has granted your request");
+                oldPositionX = token.data.x;
+                oldPositionY = token.data.y;
+            }
+            else {
+                ui.notifications.warn("The GM has declined your request");
+                oldPositionX = payload.oldX;
+                oldPositionY = payload.oldY;
+            }
+            disableMoveKeys(false);
+            duplicateCheck = false;
+            
+        }
+    });
+});
+
+Hooks.once('init', function(){
+    //initialize all settings
+    game.settings.register('NotYourTurn','BlockPlayer', {
+        name: "Player",
+        scope: "world",
+        config: true,
+        type:Number,
+        default:2,
+        choices:["Off","Warning Only","Dialog Box","Auto Block"],
+        onChange: x => window.location.reload()
+    });
+    game.settings.register('NotYourTurn','BlockTrusted', {
+        name: "Trusted",
+        scope: "world",
+        config: true,
+        type:Number,
+        default:2,
+        choices:["Off","Warning Only","Dialog Box","Auto Block"],
+        onChange: x => window.location.reload()
+    });
+    game.settings.register('NotYourTurn','BlockAssistant', {
+        name: "Assistant",
+        scope: "world",
+        config: true,
+        type:Number,
+        default:1,
+        choices:["Off","Warning Only","Dialog Box","Autoblock"],
+        onChange: x => window.location.reload()
+    });
+    game.settings.register('NotYourTurn','BlockGM', {
+        name: "GM",
+        hint: "Determines behavior when token is moved when its not its turn for each permission level",
+        scope: "world",
+        config: true,
+        type:Number,
+        default:1,
+        choices:["Off","Warning Only","Dialog Box","Auto Block"],
+        onChange: x => window.location.reload()
+    });
+
+    game.settings.register('NotYourTurn','IgnoreButton', {
+        name: "Ignore Button",
+        hint: "Determines who can see the ignore button in the dialog box",
+        scope: "world",
+        config: true,
+        type:Number,
+        default:2,
+        choices:["Everyone","Trusted & Up","Assistants & Up","Gamemaster only","Nobody"],
+        onChange: x => window.location.reload()
+    });
+    game.settings.register('NotYourTurn','RequestButton', {
+        name: "GM Request Button",
+        hint: "Allow players to request the GM to allow movement",
+        scope: "world",
+        config: true,
+        default: true,
+        type: Boolean,
+        onChange: x => window.location.reload()
+    });
+    game.settings.register('NotYourTurn','ChatMessages', {
+        name: "Chat Messages",
+        hint: "Creates a chat message when a token is moved when it's not its turn if set to 'warning only' or 'dialog box'",
+        scope: "world",
+        config: true,
+        default: true,
+        type: Boolean,
+        onChange: x => window.location.reload()
+    });
+});
+
+function disableMoveKeys(enable){
+    if (enable){
+        game.keyboard.moveKeys.w = "";
+        game.keyboard.moveKeys.a = "";
+        game.keyboard.moveKeys.s = "";
+        game.keyboard.moveKeys.d = "";
+        game.keyboard.moveKeys.W = "";
+        game.keyboard.moveKeys.A = "";
+        game.keyboard.moveKeys.S = "";
+        game.keyboard.moveKeys.D = "";
+        game.keyboard.moveKeys.ArrowUp = "";
+        game.keyboard.moveKeys.ArrowRight = "";
+        game.keyboard.moveKeys.ArrowDown = "";
+        game.keyboard.moveKeys.ArrowLeft = "";
+        game.keyboard.moveKeys.Numpad1 = "";
+        game.keyboard.moveKeys.Numpad2 = "";
+        game.keyboard.moveKeys.Numpad3 = "";
+        game.keyboard.moveKeys.Numpad4 = "";
+        game.keyboard.moveKeys.Numpad6 = "";
+        game.keyboard.moveKeys.Numpad7 = "";
+        game.keyboard.moveKeys.Numpad8 = "";
+        game.keyboard.moveKeys.Numpad9 = "";
+    }
+    else {
+        game.keyboard.moveKeys.w = ["up"];
+        game.keyboard.moveKeys.s = ["down"];
+        game.keyboard.moveKeys.a = ["left"];
+        game.keyboard.moveKeys.d = ["right"];
+        game.keyboard.moveKeys.W = ["up"];
+        game.keyboard.moveKeys.S = ["down"];
+        game.keyboard.moveKeys.A = ["left"];
+        game.keyboard.moveKeys.D = ["right"];
+        game.keyboard.moveKeys.ArrowUp = ["up"];
+        game.keyboard.moveKeys.ArrowRight = ["right"];
+        game.keyboard.moveKeys.ArrowDown = ["down"];
+        game.keyboard.moveKeys.ArrowLeft = ["left"];
+        game.keyboard.moveKeys.Numpad1 = ["down","left"];
+        game.keyboard.moveKeys.Numpad2 = ["down"];
+        game.keyboard.moveKeys.Numpad3 = ["down","right"];
+        game.keyboard.moveKeys.Numpad4 = ["left"];
+        game.keyboard.moveKeys.Numpad6 = ["right"];
+        game.keyboard.moveKeys.Numpad7 = ["up","left"];
+        game.keyboard.moveKeys.Numpad8 = ["up"];
+        game.keyboard.moveKeys.Numpad9 = ["up","right"];
+    }   
+}
+
+Hooks.on('controlToken', (token,controlled)=>{
+    if (controlled == false) return;
+    oldPositionX = token.data.x;
+    oldPositionY = token.data.y;
+    
+    Hooks.on('updateToken',(scene,data,c,d,user)=>{
+        if (blockSett == 0) return;
+        
+        //To prevent the dialog from appearing multiple times, set a timer
+        if (duplicateCheck == true) 
+           return;
+        
+        timer = Date.now();
+
+        //check if currently in combat
+        let inCombat = checkCombat();
+
+        //check if client controls the token, if the user corresponds with the client and if in combat
+        if (user != game.userId || inCombat == false) return;
+
+        for (let i=0; i<game.combat.data.combatants.length; i++)
+            if (game.combat.data.combatants[i].tokenId == data._id){
+                if (game.combat.data.combatants[i].active) {
+                    oldPositionX = data.x;
+                    oldPositionY = data.y;
+                    return;
+                }
+            }
+
+        //Check if token has moved
+        if ((data.x - oldPositionX) == 0 && (data.y - oldPositionY) == 0) return;
+
+        
+        for (let i=0; i<canvas.tokens.children[0].children.length; i++)
+            if (canvas.tokens.children[0].children[i].data._id == data._id)
+                token = canvas.tokens.children[0].children[i];
+        
+        duplicateCheck = true;
+
+        //Check if autoblock applies, which will automatically force the token back to its original position
+        if (blockSett == 3){
+            token.shiftPosition((oldPositionX - data.x)/canvas.dimensions.size,(oldPositionY - data.y)/canvas.dimensions.size,true);
+            ui.notifications.warn("It is not your turn"); 
+            timer = Date.now();
+            duplicateCheck = false;
+            
+        }
+        //Check if 'warning only' is set for the turn block function, if so, continue movement and give warning
+        else if (blockSett == 1){
+            ui.notifications.warn("It is not your turn");
+            if (game.settings.get("NotYourTurn","ChatMessages")==true && role < 3) 
+                whisperGM(token.name + " moved when it was not its turn");
+            oldPositionX = data.x;
+            oldPositionY = data.y;
+            timer = Date.now();
+            duplicateCheck = false;
+        }
+        
+        //In all other cases, create a dialog box
+        else {
+            disableMoveKeys(true);
+            //Create a dialog, with buttons based on the current situation
+            let applyChanges = 0;
+            let buttons = {
+                //Undo button
+                Undo: {
+                    label: `Undo`,
+                    callback: () => applyChanges = 0
+                }
+            }
+            //Check if the role of the user. If applicable, add ignore button
+            if (game.settings.get("NotYourTurn","IgnoreButton")<role){
+                buttons.Ignore = {
+                    label: `Ignore`,
+                    callback: () => applyChanges = 1
+                }
+            }
+            //Check if the user is player, add request button if enabled
+            if (game.settings.get("NotYourTurn","RequestButton")==true && role <4){
+                buttons.Request = {
+                    label: `Request`,
+                    callback: () => applyChanges = 2
+                }
+            }
+
+            let title = "It's not your turn!";
+            let content = ` You're trying to move, but it is not your turn<br><br>`;
+            let d = new Dialog({
+                title: "It's not your turn!",
+                content: "You're trying to move, but it is not your turn<br><br>",
+                buttons,
+                default: "Undo",
+                close: html => {
+                    //If 'Undo' is pressed, move token back to previous position
+                    if (applyChanges == 0){ //undo
+                        token.shiftPosition((oldPositionX - data.x)/canvas.dimensions.size,(oldPositionY - data.y)/canvas.dimensions.size,true);
+                        disableMoveKeys(false);
+                        duplicateCheck = false;
+                    }
+                    //If 'Ignore' is pressed, continue movement
+                    else if (applyChanges == 1) { //ignore
+                        if (game.settings.get("NotYourTurn","ChatMessages")==true && role < 3) 
+                            whisperGM(token.name + " moved when it was not its turn");                                                                                    
+                        disableMoveKeys(false);
+                        duplicateCheck = false;
+                        oldPositionX = data.x;
+                        oldPositionY = data.y;
+                    }  
+                    else if (applyChanges == 2) { //request movement
+                        //Request movement from GM, then apply movement (GM can undo this)
+                        for (let i=0; i<game.data.users.length; i++)
+                            if (game.data.users[i].role > 2) {
+                                let payload = {
+                                    "msgType": "requestMovement",
+                                    "sender": game.userId, 
+                                    "receiver": game.data.users[i]._id, 
+                                    "tokenId": token.data._id,
+                                    "oldX": oldPositionX,
+                                    "oldY": oldPositionY
+                                };
+                                game.socket.emit(`module.NotYourTurn`, payload);
+                            }
+                    }
+                }
+            });
+            d.render(true); 
+        }
+        timer = Date.now();
+    })
+})
--- README.md
+++ README.md
@@ -1,31 +1,25 @@
 # StepCounter
-Step Counter is a <a href="https://foundryvtt.com/">Foundry VTT</a> module that adds a step counter to Foundry.
-The module counts the distance that a token has moved to keep track of a token's movement, and can be used to prevent tokens from moving when they're not supposed to.
-It was developed for DnD 5e.
+'Not Your Turn' is a <a href="https://foundryvtt.com/">Foundry VTT</a> module that prevents tokens from moving when it's not their turn in combat.
 
 ### Operation
-Using the default settings, a small textbox appears on the upper left corner of the screen during combat. This box gives the distance moved, the speed of the token, and whether 'dash' has been used. If a player moves a token further than its speed allows, a dialog box appears. The step counter is automatically reset at the start of the token's turn<br>
-<br>
-This dialog box contains the following options:<br>
+If a player moves a token when it's not the token's turn one of a few things can happen, depending on the settings:
 <ul>
-<li><b>Undo</b> - Returns the token to its previous position</li>
-<li><b>Dash</b> - Applies the dash (bonus)action, to double the token's speed (can only be used once each turn)</li>
-<li><b>Reset</b> - Resets the step counter (can be disabled)</li>
-<li><b>Ignore</b> - Ignores the step counter (can be disabled)</li>
-<li><b>Request</b> - Request the GM to move the token further</li>
+<li>Nothing, the player can continue moving</li>
+<li>A warning is given</li>
+<li>A dialog box appears</li>
+<li>The token is automatically blocked from moving</li>
 </ul>
 
-Furthermore, if a token is moved when its not the token's turn, a similar dialog box appears containing the following options:<br>
+The dialog box contains the following options:<br>
 <ul>
-<li><b>Undo</b> - Same as above</li>
-<li><b>Ignore</b> - Same as above</li>
-<li><b>Request</b> - Same as above</li>
+<li><b>Undo</b> - Returns the token to its previous position</li>
+<li><b>Ignore</b> - Ignores the step counter (can be disabled)</li>
+<li><b>Request</b> - Request the GM to move the token further</li>
 </ul>
 
 If the request button is pressed, a dialog box appears for the GM, with the following options:<br>
 <ul>
 <li><b>Accept</b> - Accepts the movement request</li>
-<li><b>Accept + Reset</b> - Accepts the request and resets the step counter</li>
 <li><b>Decline</b> - Declines the request and returns the token to its start position</li>
 </ul>
 
@@ -33,26 +27,19 @@ If the request button is pressed, a dialog box appears for the GM, with the foll
 The operation of the module can be modified in the module settings. Here is an overview of the settings:<br>
 <ul>
 <li><b>Turn Block</b> - This determines the behavior when a token tries to move when it's not its turn. It can be 'Off' (nothing happens), 'Warning Only' (only a warning message is displayed), 'Dialog Box' (the above mentioned dialog box is displayed), 'Auto Block' (movement is automatically blocked). These settings can be set for each permission level (player/trusted/assistant/gamemaster).</li>
-<li><b>Step Counter</b> - This Determines the behavior when a token tries to move further than its speed allows. It can be 'Off' (nothing happens, no display), 'Display Only' (nothing happens, but the display is shown), 'Display + Warning' (a warning message is displayed, and the display is shown), 'Display + Dialog Box' (above mentioned dialog box appears, and the display is shown), 'Display + Auto Block' (movement is automatically blocked, and the display is shown). These settings can be set for each permission level.</li>
-<li><b>Step Counter Reset Button</b> - This determines what users have access to the 'Reset' button in the dialog box.</li>
-<li><b>Ignore Step Counter Button</b> - This determines what usersr have access to the 'Ignore' button in the dialog box.</li>
-<li><b>GM Request Button</b> - Adds or removes the 'Request' button (it is not needed if players have acces to the 'Reset' and 'Ignore' buttons.</li>
-<li><b>Combat Only</b> - Only enables the step counter during combat.</li>
-<li><b>Auto Reset</b> - Automatically resets the step counter a the start of a token's turn.</li>
-<li><b>Chat Messages</b> - Creates a chat message whenever a non-gm user either: presses the 'Reset' button, presses the 'Ignore' button, moves a token too far when 'Step Counter' is set to 'Display + Warning', moves a token when it's not their turn and 'Turn Block' is set to 'Warning Only'.</li>
+<li><b>Ignore Button</b> - This determines what users have access to the 'Ignore' button in the dialog box</li>
+<li><b>GM Request Button</b> - Adds or removes the 'Request' button (it is not needed if players have acces to the 'Ignore' button)</li>
+<li><b>Chat Messages</b> - Creates a chat message whenever a non-gm user either: presses the 'Ignore' button or moves a token when it's not their turn and 'Turn Block' is set to 'Warning Only'</li>
 </ul>
 
-### Macros
-There are 3 macros included as a compendium:
+### Limitations
 <ul>
-<li><b>Enable Dash</b> - Enables dash to double the movement speed</li>
-<li><b>Disable Dash</b> - Disables dash</li>
-<li><b>Reset Step Counter</b> - Resets the step counter and disables dash</li>
+<li>Currently, the module only works if one token is moved at a time</li>
+<li>When a player is waiting on the GM to answer his/her request, the player can move other tokens they control</li>
 </ul>
 
 ## Software Versions & Module Incompatibilities
 <b>Foundry VTT:</b> Tested on 0.6.6<br>
-<b>DnD5e:</b> Tested on 0.96<br>
 <b>Module Incompatibilities:</b> None known<br>
 
 ## Feedback
@@ -60,9 +47,6 @@ If you have any suggestions or bugs to report, feel free to contact me on Discor
 
 ## Credits
 <b>Author:</b> Cristian Deenen (Cris#6864 on Discord)<br>
-<b>Other:</b> The UI is based on the work of kekilla#7036 from his TokenBar module<br>
-<b>Special Thanks:</b> Many thanks to kekilla#7036 and cole#9640 for their help<br>
 
 ## Changelog
-<b>v1.0.0</b> - 08-09-2020 - Initial release<br>
-<b>v1.0.1</b> - 09-09-2020 - Minor bug fixes and added 5/10/5 diagonal movement calculations<br>
+<b>v1.0.0</b> - 13-09-2020 - Initial release<br>
